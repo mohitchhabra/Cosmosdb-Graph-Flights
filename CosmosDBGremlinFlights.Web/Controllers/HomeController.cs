5fdfe85c4d2d76144238e67fbaee7aeadb025caf
Mohit Chhabra <Mohitchhabra@techie.com>
2018-04-21 00:45:36 +0530

    Add project files.

---
+++ CosmosDBGremlinFlights.Web/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Device.Location;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Graphs;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CosmosDBGremlinFlights.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index(string from = "", string to = "")
+        {
+            ViewBag.BingMapsKey = ConfigurationManager.AppSettings["BingMapsKey"];
+            ViewBag.JourneysJson = "[]";
+
+            if (string.IsNullOrEmpty(from) || string.IsNullOrEmpty(to)) return View();
+
+            // clean input
+            from = Regex.Replace(from, @"\W", "").ToUpper();
+            to = Regex.Replace(to, @"\W", "").ToUpper();
+
+            ViewBag.From = from;
+            ViewBag.To = to;
+            
+            using (DocumentClient client = new DocumentClient(
+               new Uri(ConfigurationManager.AppSettings["CosmosDBUri"]),
+               ConfigurationManager.AppSettings["CosmosDBKey"],
+               new ConnectionPolicy { ConnectionMode = ConnectionMode.Direct, ConnectionProtocol = Protocol.Tcp }))
+            {
+                var graph = await client.ReadDocumentCollectionAsync(ConfigurationManager.AppSettings["CosmosDBCollectionLink"]);
+
+                var fromAirport = await GetAirportAsync(from, client, graph);
+                var toAirport = await GetAirportAsync(to, client, graph);
+
+                var routes = await GetRoutes(fromAirport, toAirport, client, graph);
+                ViewBag.JourneysJson = JsonConvert.SerializeObject(routes);
+            }
+            return View();
+        }
+
+        private async Task<Airport> GetAirportAsync(string code, DocumentClient client, DocumentCollection graph)
+        {
+            var query = client.CreateGremlinQuery<Document>(graph, $"g.V('{Escape(code)}')");
+            var results = await query.ExecuteNextAsync();
+            var airportVertex = results.SingleOrDefault();
+            
+            if (airportVertex == null)
+            {
+                return null;
+            }
+            else
+            {
+                return new Airport(airportVertex);
+            }
+        }
+
+        private async Task<IEnumerable<Journey>> GetRoutes(Airport fromAirport, Airport toAirport, DocumentClient client, DocumentCollection graph)
+        {
+            const double maxDistanceFactor = 1.5;
+
+            var from = Escape(fromAirport.Code);
+            var to = Escape(toAirport.Code);
+            var distance = GetDistance(fromAirport, toAirport);
+            var maxDistance = distance * maxDistanceFactor;
+
+            var query = client.CreateGremlinQuery<Document>(graph, $"g.V('{from}').union(outE().inV().hasId('{to}'), outE().inV().outE().inV().hasId('{to}')).path()");
+            IEnumerable<Journey> allJourneys = new List<Journey>(); 
+            while (query.HasMoreResults)
+            {
+                var results = await query.ExecuteNextAsync();
+                var journeys = results
+                    .Select(r => GetJourney((JArray)r.objects, maxDistance))
+                    .Where(j => j != null);
+                allJourneys = allJourneys.Concat(journeys);
+            }
+            return allJourneys;
+        }
+
+        private Journey GetJourney(JArray path, double maxDistance)
+        {
+            var totalDistance = path.Where(i => i["label"].Value<string>() == "flight").Cast<dynamic>().Sum(f => (double)f.properties.distance.Value);
+            if (totalDistance < maxDistance)
+            {
+                return new Journey
+                {
+                    TotalDistance = totalDistance,
+                    Airports = path.Where(i => i["label"].Value<string>() == "airport").Select(i => new Airport(i)).ToArray()
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private double GetDistance(Airport fromAirport, Airport toAirport)
+        {
+            var from = new GeoCoordinate(fromAirport.Latitude, fromAirport.Longitude);
+            var to = new GeoCoordinate(toAirport.Latitude, toAirport.Longitude);
+            return from.GetDistanceTo(to);
+        }
+
+        private string Escape(string input)
+        {
+            return input?.Replace("'", @"\'")?.Replace("\"", "\\\"");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Cosmos DB Gremlin graph API flights sample
+
+Create a Cosmos DB account. In the account, create a database and a graph.
+
+Configure and run the [airports and routes loader](#airports-and-routes-loader) to load data into the graph.
+
+Configure and run the [web app](#web-app).
+
+![Demo](cosmosdb-flights.gif)
+
+
+## Airports and routes loader
+
+Loads a Cosmos DB Gremlin graph database with airports and routes from [OpenFlights](https://openflights.org/data.html).
+
+### Instructions
+
+Update config file with connection information to your Cosmos DB and run it! There is a lot of data and currently the data is loaded sequentially, so it's going to take a while. Go get a coffee or lunch or something.
+
+## Web app
+
+Allows the user to enter two airport codes and find routes between the airports.
+
+### Instructions
+
+Update the web.config file with connection information to your Cosmos DB and run it! The app currently only accepts airport codes.
